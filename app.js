b70e4d541a1bc158d03ad99f7809bfd766ee1e50
Rahumanthoufic <67252533+Rahumanthoufic@users.noreply.github.com>
2022-01-02 14:43:41 +0530

    Finished code of secred app

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema= new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -93,11 +94,41 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+  User.find({secret:{$ne : null}}, function(err, users){
+    if (err){
+      console.log(err);
+    }else {
+      if (users){
+        res.render("secrets", {userSecrets: users})
+      }
+    }
+  })
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user._id, function(err, foundUser){
+    if (err){
+      res.redirect("/login");
+    } else {
+      if (foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(err){
+          if (!err){
+            res.redirect("/secrets");
+          }
+        });
+      }
+    }
+  })
 });
 
 app.get("/logout", function(req, res){
